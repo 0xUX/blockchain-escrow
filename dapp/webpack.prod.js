922148446db4dfc585360e6ecf4b097c2200272e
Mark Pors <mark@pors.net>
2018-08-23 15:51:58 +0300

    stop using purifycss, doesnt work

--- dapp/webpack.prod.js
+++ dapp/webpack.prod.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const glob = require('glob');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const PurifyCssPlugin = require('purifycss-webpack');
 const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 //const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
@@ -26,14 +27,19 @@ module.exports = merge(common, {
             }
         ]
     },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "[name].[contenthash].css"
-        }),
-        new PurifyCssPlugin({
-            purifyOptions: { minify: true },
-            // Give paths to parse for rules. These should be absolute!
-            paths: glob.sync(path.join(__dirname, 'src/**/*.js')),
         }),
         //new BundleAnalyzerPlugin()
     ]
