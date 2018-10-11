b83b5ea2747a9a1eb8d385033fa64b136a85fc9f
Stan P. van de Burgt <stan@vandeburgt.com>
2018-10-11 16:55:40 +0200

    prep for deploy on main net

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,21 +1,33 @@
 // node deploy.js
 //
-// deploy script, grabs the compiled contract and deploys it on the Rinkeby network
+// deploy script, grabs the compiled contract and deploys it on the network
+
+const util = require('util');
+const assert = require('assert');
+
+const network = process.argv[2]; // rinkeby or main
+assert.ok(network, "usage: "+process.argv[1]+" <network>");
 
 const compiledContract = require('./build/Escrow.json');
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 
-const walletKeys = require('./rinkeby-keys.json');
+//const walletKeys = require('./rinkeby-keys.json');
+const walletKeys = require(util.format('./%s-keys.json', network));
 const provider = new HDWalletProvider(walletKeys.phrase, walletKeys.api);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
     const contractFee = 5;
     accounts = await web3.eth.getAccounts();
-    console.log('Deploying on Rinkeby');
-    console.log('From:', 'https://rinkeby.etherscan.io/address/'+accounts[0]);
+    console.log('Deploying on', network);
+    if (network == "main") {
+        console.log('From:', 'https://etherscan.io/address/'+accounts[0]);
+    }
+    else {
+        console.log(util.format('From: https://%s.etherscan.io/address/%s', network, accounts[0]));
+    }
 
 //     var contract = await new web3.eth.Contract(JSON.parse(compiledContract.interface));
 //     var contractData = contract.new.getData(contractFee, {data: '0x'+compiledContract.bytecode});
